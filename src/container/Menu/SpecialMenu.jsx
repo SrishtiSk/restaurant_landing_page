9c44eb2a3b5f90e3e5962b7a669b126d10eb5d1c
SrishtiSk <unofficial.srishtisk@gmail.com>
2024-05-25 15:35:30 +0530

    Awards section

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import './SpecialMenu.css';
 import { images, data } from '../../constants';
-
 import { SubHeading, MenuItem } from '../../components';
 
 const SpecialMenu = () => (
